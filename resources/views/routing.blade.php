a999bcc584d7e88e280b677d678ffe9925ebd309
Kenneth <kenneth16.tombaga@gmail.com>
2026-04-30 12:10:02 +0800

    refactor: simplify search input placeholders across multiple views

--- resources/views/routing.blade.php
+++ resources/views/routing.blade.php
@@ -19,7 +19,7 @@
   <div class="subbar-right">
     <div class="search-bar">
       <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><circle cx="11" cy="11" r="8"/><line x1="21" y1="21" x2="16.65" y2="16.65"/></svg>
-      <input type="text" id="searchInput" placeholder="Search routing records…">
+      <input type="text" id="searchInput" placeholder="Search">
     </div>
     <div class="datetime" id="datetime"></div>
   </div>
